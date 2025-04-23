5ad6b6a851bce4e95f7b4119a5d606bf728777c6
Daniel <daneelahkalgahnov@gmail.com>
2025-04-23 13:47:03 +0200

    feat: make an installer for tests

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-source ./bin/setup.sh
+#!/bin/zsh
+TESTS_DIR=$PWD
+TESTS_FILE=$TESTS_DIR/tests.out
+echo "alias dkalgano-printf-tests='make -C $TESTS_DIR PROJECT_DIR=\$PWD && $TESTS_FILE'" >> ~/.zshrc
