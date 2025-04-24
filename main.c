a1240a730682f4de9e0c2a844e37fdb27d0bad36
Daniel <daneelahkalgahnov@gmail.com>
2025-04-24 16:23:55 +0200

    fix: memory leaks

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 00:01:31 by dkalgano          #+#    #+#             */
-/*   Updated: 2025/04/24 14:13:56 by dkalgano         ###   ########.fr       */
+/*   Updated: 2025/04/24 16:23:09 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "io_utils.h"
 #include "basic_tests.h"
+#include "test_utils.h"
 
 void	start_tests(t_test **results)
 {
@@ -60,5 +61,6 @@ int	main(void)
 	results = malloc(sizeof(t_test));
 	redirect_to_file(results, start_tests);
 	print_results(*results);
+	ft_free(results);
 	return (0);
 }
