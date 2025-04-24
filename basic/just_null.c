6de85940a4bf0af67ad7ebbb60b8c13904bc8474
Daniel <daneelahkalgahnov@gmail.com>
2025-04-24 19:30:05 +0200

    fix: new name

--- basic/just_null.c
+++ basic/just_null.c
@@ -6,7 +6,7 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 18:58:18 by dkalgano          #+#    #+#             */
-/*   Updated: 2025/04/24 19:02:27 by dkalgano         ###   ########.fr       */
+/*   Updated: 2025/04/24 19:29:48 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	just_null(t_test **results)
 	reset_output();
 	printf(NULL);
 	fflush(stdout);
-	test->res = file_to_string();
+	test->output = file_to_string();
 	test->pass = cmpres(test);
 	ft_testadd_back(results, test);
 }
