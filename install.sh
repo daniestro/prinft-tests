508c2ad2d01cbd2587d5b152bbea25a006ff219c
Daniel <daneelahkalgahnov@gmail.com>
2025-04-24 12:44:15 +0200

    feat: clean output

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 TESTS_DIR=$PWD
 TESTS_FILE=$TESTS_DIR/tests.out
-echo "alias dkalgano-printf-tests='make -C $TESTS_DIR PROJECT_DIR=\$PWD && $TESTS_FILE'" >> ~/.zshrc
+echo "alias dkalgano-printf-tests='make -C $TESTS_DIR > /dev/null PROJECT_DIR=\$PWD && $TESTS_FILE'" >> ~/.zshrc
